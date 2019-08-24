f60a765a03ecabf6b23142177c01d2f12b086304
Naman <namangateway@gmail.com>
2019-08-24 13:27:55 +0530

    Retrieving data from parse server

--- app/src/main/java/gupta/naman/instagramclone/MainActivity.java
+++ app/src/main/java/gupta/naman/instagramclone/MainActivity.java
@@ -5,17 +5,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
+    Button btn,btn2;
     EditText edtName,edtPunchSpeed,edtPunchPower,edtKickSpeed,edtKickPower;
+    TextView tv;
+    String all;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +53,51 @@ public class MainActivity extends AppCompatActivity {
                         else
                             FancyToast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_LONG,FancyToast.ERROR,false).show();
 
+                    }
+                });
+            }
+        });
+//TO RETRIEVE DATA FROM SERVER
+        tv=findViewById(R.id.textView);
+        tv.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                ParseQuery<ParseObject> parse=ParseQuery.getQuery("submit");
+                parse.getInBackground("Gaeokvcgip", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+                        if(object!=null && e==null)
+                        {
+                            tv.setText(object.get("name").toString()+"Punch Speed"+object.get("PunchSpeed"));
+                        }
+                    }
+                });
+            }
+        });
+
+        //to retrieve all objects at once
+        btn2=findViewById(R.id.button);
+        all="";
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                ParseQuery<ParseObject> parseAll=ParseQuery.getQuery("submit");
+                parseAll.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if(e==null)
+                        {
+                            if(objects.size()>0)
+                            {
+                                for(ParseObject p:objects)
+                                {
+                                    all+=p.get("name")+"\n";
+                                }
+
+                            }
+                            FancyToast.makeText(MainActivity.this,all,Toast.LENGTH_LONG,FancyToast.SUCCESS,false).show();
+
+                        }
                     }
                 });
             }
